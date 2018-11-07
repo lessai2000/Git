ebe55fc208ed923f22bb061d3a636e34a6b614da
유재규 <yoojg@emax-it.co.kr>
2018-11-07 18:01:50 +0900

    DC모터 제어 추가

--- Arduino/Bluetooth/DC/DC.ino
+++ Arduino/Bluetooth/DC/DC.ino
@@ -8,37 +8,52 @@
 
 /* DC 모터 돌리기 */
 
-const int motorPin = 9;     //상수 선언 : 모터와 연결된 핀 번호 저장
+//const int motorPin = 9;     //상수 선언 : 모터와 연결된 핀 번호 저장
+//
+//void setup()
+//{
+//	pinMode(motorPin, OUTPUT);      //9번핀을 출력용으로 설정
+//	Serial.begin(9600);             //시리얼 초기화
+//}
+//
+//void loop()
+//{
+//	spiningMotor();              //모터 작동시키는 함수 호출
+//}
+///*
+// 모터 작동시키는 함수
+// : 입력받은 값을 시리얼 포트를 이용해 보내고 그에 맞는 속도로 모터가 돕니다.
+//*/
+//void spiningMotor()
+//{
+//	int speed;       //변수 선언 : 입력된 속도 저장
+//	Serial.println("Write speed : 0 ~ 255 and click send");    //속도 입력하고 send 버튼을 클릭하라고 알려줌
+//
+//	while (true)    //무한반복
+//	{
+//		while (Serial.available() > 0)     //입력된 문자가 있으면
+//		{
+//			speed = Serial.parseInt();     //문자를 숫자로 변환하고 변수에 저장
+//			speed = constrain(speed, 0, 255);     //속도를 PWM 출력 값 범위로 고정
+//			analogWrite(motorPin, speed);         //speed만큼으로 모터 돌리기
+//			//speed : 값을 출력
+//			Serial.print("speed : ");
+//			Serial.println(speed);
+//		}
+//	}
+//}
+const int DC_APin = 1;
+const int DC_DPin = 11;
 
-void setup()
+void Setup()
 {
-	pinMode(motorPin, OUTPUT);      //9번핀을 출력용으로 설정
-	Serial.begin(9600);             //시리얼 초기화
+	pinMode(DC_APin, INPUT);		//A0핀에 연결된 부품 입력 설정
+	pinMode(DC_DPin, OUTPUT);	// 11핀에 연결된 부품 출력 설정
 }
 
 void loop()
 {
-	spiningMotor();              //모터 작동시키는 함수 호출
-}
-/*
- 모터 작동시키는 함수
- : 입력받은 값을 시리얼 포트를 이용해 보내고 그에 맞는 속도로 모터가 돕니다.
-*/
-void spiningMotor()
-{
-	int speed;       //변수 선언 : 입력된 속도 저장
-	Serial.println("Write speed : 0 ~ 255 and click send");    //속도 입력하고 send 버튼을 클릭하라고 알려줌
-
-	while (true)    //무한반복
-	{
-		while (Serial.available() > 0)     //입력된 문자가 있으면
-		{
-			speed = Serial.parseInt();     //문자를 숫자로 변환하고 변수에 저장
-			speed = constrain(speed, 0, 255);     //속도를 PWM 출력 값 범위로 고정
-			analogWrite(motorPin, speed);         //speed만큼으로 모터 돌리기
-			//speed : 값을 출력
-			Serial.print("speed : ");
-			Serial.println(speed);
-		}
-	}
+	int resistance = analogRead(A0);
+	int motor = map(resistance, 0, 1023, 0, 255);
+	analogWrite(11, motor);
 }
--- Arduino/Bluetooth/Servo/Servo.ino
+++ Arduino/Bluetooth/Servo/Servo.ino
@@ -9,65 +9,37 @@
 
 Servo myServo;
 
-//int rotate_Angle;
-//
-//int pinNum = 7;
-//int rotate_Speed = 100;
-//
-//void setup() {
-//	//pinMode(pinNum, OUTPUT);
-//	myServo.attach(pinNum);
-//	Serial.begin(9600);
-//	rotate_Angle = 90;
-//	myServo.write(rotate_Angle);
-//}
-//void loop() {
-//
-//	if (Serial.available)
-//	{
-//		if (Serial.read() == 97) // 아스키(d)
-//		{
-//			rotate(false);
-//		}
-//		if (Serial.read() == 100) // 아스키(a)
-//		{
-//			rotate(true);
-//		}
-//	}
-//}
-//
-//void rotate(boolean r_value)
-//{
-//	// RIGHT
-//	if (r_value) 
-//	{
-//		myServo.write(rotate_Angle + 1);
-//		delay(rotate_Speed);
-//	}
-//	//LEFT
-//	else { 
-//		myServo.write(rotate_Angle - 1);
-//		delay(rotate_Speed);
-//	}
-//}
-
-
-const int potentiometerPin = 0; //가변저항기에 연결한 아날로그의 핀
-
-Servo myServo;
+const int servo_APin = 0; //가변저항기에 연결한 아날로그의 핀
+const int servo_DPin = 9;
+const int DC_APin = 1;
+const int DC_DPin = 11;
 
 void setup()
 {
 	myServo.attach(9); // 서보를 사용하는 핀
+	pinMode(DC_APin, INPUT);
+	pinMode(DC_DPin, OUTPUT);
 }
 
 void loop()
 {
-	int value = analogRead(potentiometerPin); // 가변저항의 값
+	servo_control(analogRead(servo_APin)); // 가변저항의 값
+	DC_control(analogRead(DC_APin));
+	
+}
 
+// 서보모터 제어
+void servo_control(int value)
+{
 	// map(value, 현재 최소값, 현재 최대값, 변경하고자 하는 최소 값, 변경하고자 하는 최대값)
 	int angle = map(value, 0, 1023, 0, 179); // 읽어들인 값을 0에서 179까지 변환   
 
 	myServo.write(angle);
 	delay(15);
+}
+
+void DC_control(int value)
+{
+	int motor = map(value, 0, 1023, 0, 255);
+	analogWrite(DC_DPin, motor);
 }
