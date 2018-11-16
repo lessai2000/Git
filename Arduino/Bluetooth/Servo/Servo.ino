21b481bbee91247d0cc101837eb3167782d4deba
유재규 <yoojg@emax-it.co.kr>
2018-11-16 15:18:38 +0900

    블루투스 추가

--- Arduino/Bluetooth/Servo/Servo.ino
+++ Arduino/Bluetooth/Servo/Servo.ino
@@ -5,6 +5,7 @@
 */
 
 // the setup function runs once when you press reset or power the board
+#include <SoftwareSerial.h>
 #include <Servo.h>
 
 Servo myServo;
@@ -14,22 +15,41 @@ const int servo_DPin = 9;	//Servo motor Pin number
 const int DC_APin = 1;		//DC motor connetion potentiometer Pin number
 const int DC_DPin = 11;		//DC motor Pin number
 
+#define BT_RXD 8
+#define BT_TXD 7
+SoftwareSerial bluetooth(BT_RXD, BT_TXD);
+
 void setup()
 {
 	myServo.attach(servo_DPin); 
 	pinMode(DC_APin, INPUT);	
 	pinMode(DC_DPin, OUTPUT);
+	Serial.begin(9600);
+	bluetooth.begin(9600);
 }
 
 void loop()
 {
-	servo_control(analogRead(servo_APin));	// Servo motor potentiometer
+	Bluetooth_connect();					// Bluetooth Connect
+	Servo_control(analogRead(servo_APin));	// Servo motor potentiometer
 	DC_control(analogRead(DC_APin));		// DC motor potentiometer
 	
 }
 
+// 블루투스 제어
+void Bluetooth_connect()
+{
+	if (bluetooth.available()) {
+		// 값이 있을경우,
+		Serial.write(bluetooth.read());
+	}
+	if (Serial.available()) {
+		bluetooth.write(Serial.read());
+	}
+}
+
 // 서보모터 제어
-void servo_control(int value)
+void Servo_control(int value)
 {
 	// map(value, 현재 최소값, 현재 최대값, 변경하고자 하는 최소 값, 변경하고자 하는 최대값)
 	int angle = map(value, 0, 1023, 0, 170); // 읽어들인 값을 0에서 170까지 변환   
